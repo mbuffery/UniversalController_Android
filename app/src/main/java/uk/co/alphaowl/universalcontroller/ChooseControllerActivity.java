0545e3ca447048041736748e5e9e83941acfc517
dwestaway <dan.westaway@students.plymouth.ac.uk>
2017-11-08 12:56:19 +0000

    Implemented controller selection layout

--- app/src/main/java/uk/co/alphaowl/universalcontroller/ChooseControllerActivity.java
+++ app/src/main/java/uk/co/alphaowl/universalcontroller/ChooseControllerActivity.java
@@ -1,17 +1,13 @@
 package uk.co.alphaowl.universalcontroller;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class ChooseControllerActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        Intent intent = new Intent(this, ChooseControllerActivity.class);
-        startActivity(intent);
+        setContentView(R.layout.activity_choose_controller);
     }
 }
