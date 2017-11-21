8406ef77be00f519ff5630add92f8ad118cd5f99
Dan Westaway <dan.westaway@students.plymouth.ac.uk>
2017-11-21 17:20:26 +0000

    Added Dpad Button
    Created Dpad Buttons
    Linked button to dpad button activity

--- app/src/main/java/uk/co/alphaowl/universalcontroller/ChooseControllerActivity.java
+++ app/src/main/java/uk/co/alphaowl/universalcontroller/ChooseControllerActivity.java
@@ -1,13 +1,38 @@
 package uk.co.alphaowl.universalcontroller;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class ChooseControllerActivity extends AppCompatActivity {
 
+    ImageButton button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_controller);
     }
+
+
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        button= (ImageButton)findViewById(R.id.btn_controller1);
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Intent i = new Intent(getApplicationContext(),DpadControllerActivity.class);
+                startActivity(i);
+            }
+        });
+
+    }
 }
+
