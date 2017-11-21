330132ad632467904789fd7e04b90958fce7a3db
Dan Westaway <dan.westaway@students.plymouth.ac.uk>
2017-11-21 16:10:40 +0000

    Controller Activities
    Added activitiers for joystick and dpad controllers, make both landscape only

---
+++ app/src/main/java/uk/co/alphaowl/universalcontroller/DpadControllerActivity.java
@@ -0,0 +1,13 @@
+package uk.co.alphaowl.universalcontroller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DpadControllerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dpad_controller);
+    }
+}
---
+++ app/src/main/java/uk/co/alphaowl/universalcontroller/JoystickControllerActivity.java
@@ -0,0 +1,13 @@
+package uk.co.alphaowl.universalcontroller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class JoystickControllerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_joystick_controller);
+    }
+}
