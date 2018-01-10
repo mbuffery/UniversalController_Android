8c7dc95892ca2521a7f270a705c8b4dbb563db9b
Miles Buffery <miles.buffery@students.plymouth.ac.uk>
2018-01-10 09:07:51 +0000

    currently fixing the joystick
    issues with null reference on load up

--- app/src/main/java/uk/co/alphaowl/universalcontroller/JoystickControllerActivity.java
+++ app/src/main/java/uk/co/alphaowl/universalcontroller/JoystickControllerActivity.java
@@ -2,13 +2,36 @@ package uk.co.alphaowl.universalcontroller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import io.github.controlwear.virtual.joystick.android.JoystickView;
+
+
 
 public class JoystickControllerActivity extends AppCompatActivity {
+
+    //private TextView mTextViewAngle;
+    //private TextView mTextViewStrength;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_joystick_controller);
+
+        //mTextViewAngle = (TextView) findViewById(R.id.textView_angle);
+        //mTextViewStrength = (TextView) findViewById(R.id.textView_strength);
+
+        JoystickView joystickView = findViewById(R.id.joystickView);
+        joystickView.setOnMoveListener(new JoystickView.OnMoveListener() {
+            @Override
+            public void onMove(int angle, int strength) {
+                //mTextViewAngle.setText(angle + "°");
+                //mTextViewStrength.setText(strength + "%");
+            }
+        });
+
     }
+
 
 }
